e4978cda52c92edc4cb812ef85a8f9d6aa8ab806
ApplePod <koreasomoon@naver.com>
2023-04-22 15:01:07 +0900

    control interface

--- Pinch App/Students/Pinch/Pinch/Screen/ContentView.swift
+++ Pinch App/Students/Pinch/Pinch/Screen/ContentView.swift
@@ -87,6 +87,62 @@ struct ContentView: View {
 
             )
             
+            // MARK: - Controls
+            .overlay(
+                Group {
+                    HStack {
+                        // Scale donw
+                       
+                        Button {
+                            withAnimation(.spring()){
+                                if imageScale > 1 {
+                                    imageScale -= 1
+                                    
+                                    if imageScale <= 1 {
+                                        resetImageState()
+                                    }
+                                }
+                            }
+                        } label: {
+                            ControlImageView(icon: "minus.magnifyingglass")
+                        }
+
+                        
+                        // Reset
+                        
+                        Button {
+                            resetImageState()
+                        } label: {
+                            ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+                        }
+                        
+                        // Scale up
+                        
+                        Button {
+                            withAnimation(.spring()) {
+                                if imageScale < 5 {
+                                    imageScale += 1
+                                    
+                                    if imageScale > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                            }
+                            
+                        } label: {
+                            ControlImageView(icon: "plus.magnifyingglass")
+                        }
+                    }
+                    .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20 ))
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(12)
+                    .opacity(isAnimationg ? 1 : 0)
+                }
+                    .padding(.bottom, 30)
+                , alignment: .bottom
+            
+            )
+            
         }//: Navigation
         .navigationViewStyle(.stack)
     }
